b9bec7d6d87ee3db5f12a8639edd14a3d203943e
fsvreddit <john@mclusky.co.uk>
2023-09-15 09:03:15 +0100

    Add eslint and editorconfig and fix issues identified

--- src/main.ts
+++ src/main.ts
@@ -1,147 +1,144 @@
-import { Devvit, ModMailConversationState, User } from '@devvit/public-api';
-import { createUserSummaryModmail } from './modmail.js';
+import {Devvit, ModMailConversationState} from "@devvit/public-api";
+import {createUserSummaryModmail} from "./modmail.js";
 
 Devvit.addSettings([
-  {
-    type: 'boolean',
-    name: 'includeToolboxNotes',
-    label: 'Include Toolbox usernotes in Modmail User Summary',
-    helpText: 'If you do not use Toolbox usernotes, or have migrated away from them, including them in the modmail summary may be misleading.',
-    defaultValue: false
-  },
-  {
-    type: 'number',
-    name: 'numberOfSubsToIncludeInSummary',
-    label: 'Number of subreddits to include in comment summary',
-    helpText: 'Limit the number of subreddits listed to this number. If a user participates in lots of subreddits, a large number might be distracting',
-    defaultValue: 10,
-    onValidate: async ({ value }) => {
-      if (!value || value < 0 || value > 100) {
-        return 'Value must be between 0 and 100';
-      }
-    }
-  },
-  {
-    type: 'select',
-    name: 'subHistoryDisplayStyle',
-    label: 'Output style for subreddit history',
-    options: [
-      { label: 'Bulleted list (one subreddit per line)', value: 'bullet'},
-      { label: 'Single paragraph (all subreddits on one line - more compact)', value: 'singlepara'},
-    ],
-    multiSelect: false,
-  },
-  {
-    type: 'number',
-    name: 'numberOfCommentsToInclude',
-    label: 'Number of recently removed comments to show in summary',
-    defaultValue: 3,
-    onValidate: async ({ value }) => {
-      if (!value || value < 0 || value > 100) {
-        return 'Value must be between 0 and 100';
-      }
-    }
-  },
-  {
-    type: 'boolean',
-    name: 'createSummaryForModerators',
-    label: 'Create modmail summary when receiving modmail from subreddit moderators',
-    defaultValue: false
-  },
-  {
-    type: 'string',
-    name: 'usernamesToIgnore',
-    label: 'Do not create summaries for these users',
-    helpText: 'Comma-separated, not case sensitive',
-    defaultValue: 'Automoderator,ModSupportBot'
-  },
-  {
-    type: 'select',
-    name: 'localeForDateOutput',
-    label: 'Format for date output',
-    options: [
-      {value: "en-GB", label: "date/month/year"},
-      {value: "en-US", label: "month/date/year"},
-      {value: "ja-JP", label: "year/month/date"}
-    ],
-    multiSelect: false,
-    onValidate: async ({ value }) => {
-      if (!value)
-        "You must select a date format"
-    }
-  }
+    {
+        type: "boolean",
+        name: "includeToolboxNotes",
+        label: "Include Toolbox usernotes in Modmail User Summary",
+        helpText: "If you do not use Toolbox usernotes, or have migrated away from them, including them in the modmail summary may be misleading.",
+        defaultValue: false,
+    },
+    {
+        type: "number",
+        name: "numberOfSubsToIncludeInSummary",
+        label: "Number of subreddits to include in comment summary",
+        helpText: "Limit the number of subreddits listed to this number. If a user participates in lots of subreddits, a large number might be distracting",
+        defaultValue: 10,
+        onValidate: async ({value}) => {
+            if (!value || value < 0 || value > 100) {
+                return "Value must be between 0 and 100";
+            }
+        },
+    },
+    {
+        type: "select",
+        name: "subHistoryDisplayStyle",
+        label: "Output style for subreddit history",
+        options: [
+            {label: "Bulleted list (one subreddit per line)", value: "bullet"},
+            {label: "Single paragraph (all subreddits on one line - more compact)", value: "singlepara"},
+        ],
+        multiSelect: false,
+    },
+    {
+        type: "number",
+        name: "numberOfCommentsToInclude",
+        label: "Number of recently removed comments to show in summary",
+        defaultValue: 3,
+        onValidate: async ({value}) => {
+            if (!value || value < 0 || value > 100) {
+                return "Value must be between 0 and 100";
+            }
+        },
+    },
+    {
+        type: "boolean",
+        name: "createSummaryForModerators",
+        label: "Create modmail summary when receiving modmail from subreddit moderators",
+        defaultValue: false,
+    },
+    {
+        type: "string",
+        name: "usernamesToIgnore",
+        label: "Do not create summaries for these users",
+        helpText: "Comma-separated, not case sensitive",
+        defaultValue: "Automoderator,ModSupportBot",
+    },
+    {
+        type: "select",
+        name: "localeForDateOutput",
+        label: "Format for date output",
+        options: [
+            {value: "en-GB", label: "date/month/year"},
+            {value: "en-US", label: "month/date/year"},
+            {value: "ja-JP", label: "year/month/date"},
+        ],
+        multiSelect: false,
+        onValidate: async ({value}) => {
+            if (!value) {
+                "You must select a date format";
+            }
+        },
+    },
 ]);
 
 Devvit.addTrigger({
-  event: 'ModMail',
-  async onEvent(event, context) {
+    event: "ModMail",
+    async onEvent (event, context) {
+        console.log("Received modmail trigger event.");
 
-    console.log(`Received modmail trigger event.`);
+        const conversationResponse = await context.reddit.modMail.getConversation({
+            conversationId: event.conversationId,
+        });
 
-    var conversationResponse = await context.reddit.modMail.getConversation({
-      conversationId: event.conversationId
-    });
+        if (!conversationResponse.conversation) {
+            return;
+        }
 
-    if (!conversationResponse.conversation)
-      return;
+        // Ensure that we are responding to the first message in the chain - only want to create a summary once.
+        if (!conversationResponse.conversation.numMessages || conversationResponse.conversation.numMessages > 1) {
+            return;
+        }
 
-    // Ensure that we are responding to the first message in the chain - only want to create a summary once.
-    if (!conversationResponse.conversation.numMessages || conversationResponse.conversation.numMessages > 1)
-      return;
+        // Ensure that the modmail has a participant i.e. is about a user, and not a sub to sub modmail or internal discussion
+        if (!conversationResponse.conversation.participant || !conversationResponse.conversation.participant.name) {
+            console.log("There is no participant for the modmail conversation e.g. internal mod discussion");
+            return;
+        }
 
-    // Ensure that the modmail has a participant i.e. is about a user, and not a sub to sub modmail or internal discussion
-    if (!conversationResponse.conversation.participant || !conversationResponse.conversation.participant.name)
-    {
-      console.log("There is no participant for the modmail conversation e.g. internal mod discussion");
-      return;
-    }
+        // Check to see if conversation is already archived e.g. from a ban message
+        const conversationIsArchived = conversationResponse.conversation.state === ModMailConversationState.Archived;
 
-    // Check to see if conversation is already archived e.g. from a ban message
-    var conversationIsArchived = (conversationResponse.conversation.state == ModMailConversationState.Archived);
+        // Get the details of the user who is the "participant" (i.e. the subject of the modmail, even if they aren't the OP)
+        const user = await context.reddit.getUserByUsername(conversationResponse.conversation.participant.name);
+        const subReddit = await context.reddit.getSubredditById(context.subredditId);
 
-    // Get the details of the user who is the "participant" (i.e. the subject of the modmail, even if they aren't the OP)
-    const user = await context.reddit.getUserByUsername(conversationResponse.conversation.participant.name);
-    const subReddit = await context.reddit.getSubredditById(context.subredditId);
+        // Check if user is on the ignore list.
+        const usersToIgnore = await context.settings.get<string>("usernamesToIgnore");
+        if (usersToIgnore) {
+            const userList = usersToIgnore.split(",");
+            if (userList.some(x => x.trim().toLowerCase() === user.username.toLowerCase())) {
+                console.log(`User /u/${user.username} is on the ignore list, skipping`);
+                return;
+            }
+        }
 
-    // Check if user is on the ignore list.
-    const usersToIgnore = await context.settings.get<string>('usernamesToIgnore');
-    if (usersToIgnore)
-    {
-      const userList = usersToIgnore.split(',');
-      if (userList.some(x => x.trim().toLowerCase() == user.username.toLowerCase()))
-      {
-        console.log(`User /u/${user.username} is on the ignore list, skipping`);
-        return;
-      }
-    }
+        // Check if user is a mod, and if app is configured to send summaries for mods
+        const createSummaryForModerators = await context.settings.get<boolean>("createSummaryForModerators");
+        if (conversationResponse.conversation.participant.isMod && !createSummaryForModerators) {
+            console.log(`${user.username} is a moderator of /r/${subReddit.name}, skipping`);
+            return;
+        }
 
-    // Check if user is a mod, and if app is configured to send summaries for mods
-    const createSummaryForModerators = await context.settings.get<boolean>('createSummaryForModerators');
-    if (conversationResponse.conversation.participant.isMod && !createSummaryForModerators)
-    {
-        console.log(`${user.username} is a moderator of /r/${subReddit.name}, skipping`);
-        return;
-    }
+        // All checks passed. Retrieve text for modmail summary.
+        const modmailMessage = await createUserSummaryModmail(context, user, subReddit.name);
 
-    // All checks passed. Retrieve text for modmail summary.
-    const modmailMessage = await createUserSummaryModmail(context, user, subReddit.name);
+        await context.reddit.modMail.reply({
+            body: modmailMessage,
+            conversationId: event.conversationId,
+            isInternal: true,
+        });
 
-    await context.reddit.modMail.reply({
-      body: modmailMessage,
-      conversationId: event.conversationId,
-      isInternal: true
-    });
-
-    // If conversation was previously archived (e.g. a ban) archive it again.
-    if (conversationIsArchived)
-    {
-      await context.reddit.modMail.archiveConversation(event.conversationId);
-    }
-  }
+        // If conversation was previously archived (e.g. a ban) archive it again.
+        if (conversationIsArchived) {
+            await context.reddit.modMail.archiveConversation(event.conversationId);
+        }
+    },
 });
 
 Devvit.configure({
-  redditAPI: true
+    redditAPI: true,
 });
 
 export default Devvit;
--- src/modmail.ts
+++ src/modmail.ts
@@ -1,98 +1,83 @@
-import { TriggerContext, User } from '@devvit/public-api';
-import { formatDistanceToNow } from 'date-fns';
-import { ToolboxClient } from 'toolbox-devvit';
+import {TriggerContext, User} from "@devvit/public-api";
+import {formatDistanceToNow} from "date-fns";
+import {ToolboxClient} from "toolbox-devvit";
 
-
-export async function createUserSummaryModmail(context: TriggerContext, user: User, subredditName: string): Promise<string>
-{
+export async function createUserSummaryModmail (context: TriggerContext, user: User, subredditName: string): Promise<string> {
     console.log("About to create summary modmail");
-    var modmailMessage = `Possible relevant information for /u/${user.username}:\n\n`;
+    let modmailMessage = `Possible relevant information for /u/${user.username}:\n\n`;
 
-    modmailMessage += `**Age**: ${formatDistanceToNow(user.createdAt)}\n\n`
+    modmailMessage += `**Age**: ${formatDistanceToNow(user.createdAt)}\n\n`;
 
     modmailMessage += `**Karma**: Post ${user.linkKarma}, Comment ${user.commentKarma}\n\n`;
 
-    if (user.nsfw)
+    if (user.nsfw) {
         modmailMessage += "**NSFW Account**: Yes\n\n";
+    }
 
     const userComments = await user.getComments({
         sort: "new",
-        limit: 100
+        limit: 100,
     }).all();
 
     // Build up a list of subreddits and the count of comments in those subreddits
-    var commentList = new Array<{subName: string, commentCount: number}>;
-    for (var comment of userComments)
-    {
-        var item = commentList.find(x => x.subName == comment.subredditName);
-        if (item)
-        {
+    let commentList: Array<{subName: string, commentCount: number}> = [];
+    for (const comment of userComments) {
+        const item = commentList.find(x => x.subName === comment.subredditName);
+        if (item) {
             // Item already in array - increment
             item.commentCount++;
-        }
-        else
-        {
+        } else {
             // First comment for this subreddit - insert new item into array
             commentList.push({
                 subName: comment.subredditName,
-                commentCount: 1
+                commentCount: 1,
             });
         }
     }
 
-    var numberOfSubsToReportOn = await context.settings.get<number>('numberOfSubsToIncludeInSummary');
-    if (!numberOfSubsToReportOn)
-        numberOfSubsToReportOn = 10;
+    const numberOfSubsToReportOn = await context.settings.get<number>("numberOfSubsToIncludeInSummary") ?? 10;
 
     commentList = commentList
         .sort((a, b) => b.commentCount - a.commentCount) // Sort descending...
         .slice(0, numberOfSubsToReportOn); // Then take top N entries
 
-    if (commentList.length > 0)
-    {
-        const subHistoryDisplayStyle = await context.settings.get<string>('subHistoryDisplayStyle') ?? 'bullet';
+    if (commentList.length > 0) {
+        const subHistoryDisplayStyle = await context.settings.get<string>("subHistoryDisplayStyle") ?? "bullet";
         modmailMessage += "**Recent Comments**: ";
-        if (subHistoryDisplayStyle == 'bullet') {
+        if (subHistoryDisplayStyle === "bullet") {
             modmailMessage += "\n\n";
         }
-        
-        for (const item of commentList)
-        {
-            if (subHistoryDisplayStyle == 'bullet') {
+
+        for (const item of commentList) {
+            if (subHistoryDisplayStyle === "bullet") {
                 modmailMessage += `* /r/${item.subName}: ${item.commentCount}\n`;
             } else {
                 modmailMessage += `/r/${item.subName} (${item.commentCount}), `;
             }
         }
-        if (subHistoryDisplayStyle == 'bullet') {
+
+        if (subHistoryDisplayStyle === "bullet") {
             modmailMessage += "\n";
         } else {
             // Remove trailing comma, add newlines
-            modmailMessage = modmailMessage.substring(0, modmailMessage.length - 2) + "\n\n";
+            modmailMessage = `${modmailMessage.substring(0, modmailMessage.length - 2)}\n\n`;
         }
     }
 
-    var locale = await context.settings.get<string>('localeForDateOutput');
-    if (!locale)
-        locale = "en-GB";
+    const locale = await context.settings.get<string>("localeForDateOutput") ?? "en-GB";
 
-    var numberOfRemovedCommentsToInclude = await context.settings.get<number>('numberOfCommentsToInclude');
-    if (!numberOfRemovedCommentsToInclude)
-        numberOfRemovedCommentsToInclude = 3;
+    const numberOfRemovedCommentsToInclude = await context.settings.get<number>("numberOfCommentsToInclude") ?? 3;
 
-    if (numberOfRemovedCommentsToInclude > 0)
-    {
+    if (numberOfRemovedCommentsToInclude > 0) {
         const filteredComments = userComments
-            .filter(x => x.removed && x.subredditName == subredditName)
+            .filter(x => x.removed && x.subredditName === subredditName)
             .slice(0, numberOfRemovedCommentsToInclude);
 
-        if (filteredComments.length > 0)
-        {
+        if (filteredComments.length > 0) {
             modmailMessage += "**Recently removed comments**:\n\n";
 
-            for (const comment of filteredComments)
-            {
-                modmailMessage += `[${comment.createdAt.toLocaleDateString(locale)}](${comment.permalink}):\n\n`
+            for (const comment of filteredComments) {
+                modmailMessage += `[${comment.createdAt.toLocaleDateString(locale)}](${comment.permalink}):\n\n`;
                 modmailMessage += `> ${comment.body.split("\n").join("\n> ")}\n\n`; // string.replaceAll not available without es2021
             }
 
@@ -100,28 +85,22 @@ export async function createUserSummaryModmail(context: TriggerContext, user: Us
         }
     }
 
-    const shouldIncludeUsernotes = await context.settings.get<boolean>('includeToolboxNotes');
-    if (shouldIncludeUsernotes)
-    {
+    const shouldIncludeUsernotes = await context.settings.get<boolean>("includeToolboxNotes");
+    if (shouldIncludeUsernotes) {
         const toolbox = new ToolboxClient(context.reddit);
-        try
-        {
-            var userNotes = await toolbox.getUsernotesOnUser(subredditName, user.username);
-            if (userNotes.length > 0)
-            {
+        try {
+            const userNotes = await toolbox.getUsernotesOnUser(subredditName, user.username);
+            if (userNotes.length > 0) {
                 modmailMessage += "**Toolbox Usernotes**:\n\n";
-                for (const note of userNotes)        
-                {
-                    var modnote = "";
-                    if (note.noteType && note.noteType != "")
+                for (const note of userNotes) {
+                    let modnote = "";
+                    if (note.noteType && note.noteType !== "") {
                         modnote += `[${note.noteType}] `;
-
-                    if (note.contextPermalink && note.contextPermalink != "")
-                    {
-                        modnote += `[${note.text}](${note.contextPermalink})`
                     }
-                    else
-                    {
+
+                    if (note.contextPermalink && note.contextPermalink !== "") {
+                        modnote += `[${note.text}](${note.contextPermalink})`;
+                    } else {
                         modnote += note.text;
                     }
 
@@ -131,13 +110,11 @@ export async function createUserSummaryModmail(context: TriggerContext, user: Us
                 }
                 modmailMessage += "\n";
             }
-        }
-        catch(e)
-        {
-            console.log("Failed to retrieve Toolbox usernotes. The Toolbox wiki page may not exist on this subreddit.")
+        } catch (e) {
+            console.log("Failed to retrieve Toolbox usernotes. The Toolbox wiki page may not exist on this subreddit.");
         }
     }
-      
+
     console.log(modmailMessage);
 
     return modmailMessage;
